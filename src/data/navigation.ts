f77d23d6a4342973d52d66397f2f269debadb176
Terrell Pelletier <155713484+hiyouvreachedty@users.noreply.github.com>
2026-04-22 18:40:38 -0700

    chore: add src/data/navigation.ts

---
+++ src/data/navigation.ts
@@ -0,0 +1,7 @@
+// Navigation links
+export const navLinks = [
+  { label: 'Work', href: '#work' },
+  { label: 'Services', href: '#services' },
+  { label: 'About', href: '#about' },
+  { label: 'Contact', href: '#contact' },
+];
